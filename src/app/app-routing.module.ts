64bc9d3570de97113fbb168a04959e8ad83e99c8
Jose Rafael Guaura <jguaura@gmail.com>
2023-03-07 19:36:18 -0500

    feat:
    - not-found/404 component + redirection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
+import { NotFoundComponent } from './not-found/not-found.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { NgModule } from '@angular/core';
 import { RecipeComponent } from './recipe-book/recipe-book.component';
 import { RouterModule, Routes } from "@angular/router";
 
 const appRoutes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipeComponent },
   { path: 'shopping-list', component: ShoppingListComponent },
-  { path: '**', redirectTo: '/recipes' }
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
 ]
 
 @NgModule({
