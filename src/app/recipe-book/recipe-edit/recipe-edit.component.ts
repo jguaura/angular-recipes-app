dac6993aac4bdd35cecb0effcb5ac98cd810c5d4
Jose Rafael Guaura <jguaura@gmail.com>
2023-03-10 18:42:02 -0500

    chore:
    - new recipe & edit mode navigation

--- src/app/recipe-book/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe-book/recipe-edit/recipe-edit.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html'
 })
-export class RecipeEditComponent {
-
+export class RecipeEditComponent implements OnInit {
+  id: number;
+  isEditing: boolean = false;
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id']
+          this.isEditing = params['id'] !== null;
+          console.log(this.isEditing)
+        }
+      )
+  }
 }
