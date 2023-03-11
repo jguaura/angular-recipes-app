da57a347bbd33a860b8320cab56a38b3d92e8ab8
Jose Rafael Guaura <jguaura@gmail.com>
2023-03-11 17:50:52 -0500

    refactor:
    - replaced event emitter weith subject in shopping list service

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,22 +1,29 @@
 import { ShoppingListService } from './shopping-list.service';
 import { Ingredient } from '../shared/ingredient.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html'
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private ingredientsSubscription: Subscription;
 
   constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit(): void {
-    this.ingredients = this.shoppingListService.getIngredients()
-    this.shoppingListService.ingredientsChanged.subscribe(
-    (ingredients: Ingredient[]) => {
-      this.ingredients = ingredients;
-    }
-    )
+    this.ingredients = this.shoppingListService.getIngredients();
+    this.ingredientsSubscription =
+      this.shoppingListService.ingredientsChanged.subscribe(
+        (ingredients: Ingredient[]) => {
+          this.ingredients = ingredients;
+        }
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.ingredientsSubscription.unsubscribe();
   }
 }
